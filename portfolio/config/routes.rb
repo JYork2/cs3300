2bb952e589c320fd909c61ef418a58024e804cb6
JYork2 <jyork2@uccs.edu>
2022-04-13 23:51:09 -0600

    restarted project ro remove duplicate column :user error

--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   #devise_for :users
   
-  devise_for :users, controllers: {
-      sessions: 'users/sessions'
-    }
+  
+  #devise_for :users, controllers: {
+  #    sessions: 'users/sessions'
+  #  }
   #devise_for :users
   root to: "projects#index"
   resources :projects
