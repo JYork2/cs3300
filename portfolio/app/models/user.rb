6a69358c02dd17743f3f76016ca1e787b85b9bdf
JYork2 <jyork2@uccs.edu>
2022-04-13 23:29:08 -0600

    More work

--- portfolio/app/models/user.rb
+++ portfolio/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable, stretches: 13
 end
--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "projects#index"
+  #devise_for :users
+  
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  #devise_for :users
+  root to: "projects#index"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    
   # Defines the root path route ("/")
   # root "articles#index"
 end
